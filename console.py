1e46716895033bd1729d149c5667b9dbecb1db73
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-02-03 13:54:42 +0000

    Console set up and user_repository funtions added

---
+++ console.py
@@ -0,0 +1,6 @@
+from models.user import User
+from models.transaction import Transaction
+
+import repositories.transaction_repository as transaction_repository
+import repositories.user_repository as user_repository
+
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -2,3 +2,63 @@ from db.run_sql import run_sql
 
 from models.user import User
 from models.transaction import Transaction
+
+def save(user):
+    sql = "INSERT INTO users (first_name, last_name) VALUES (%s, %s) RETURNING *"
+    values = [user.first_name, user.last_name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    user.id = id
+    return user
+
+
+def select_all():
+    users = []
+
+    sql = "SELECT * FROM users"
+    results = run_sql(sql)
+
+    for row in results:
+        user = (
+            row['first_name'],
+            row['last_name'],
+            row['id']
+        )
+        users.append(user)
+    return users
+
+
+def delete_all():
+    sql = "DELETE FROM users"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM users WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(user):
+    sql = "UPDATE users SET (first_name, last_name) = (%s, %s) WHERE id = %s"
+    values = [user.id]
+    run_sql(sql, values)
+
+
+def transactions(user):
+    transactions = []
+
+    sql = "SELECT * FROM transactions WHERE user_id = %s"
+    values = [user.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        transaction = Transaction(
+            row['merchant'],
+            row['category'],
+            row['amount'],
+            row['user_id'],
+            row['id']
+        )
+        transactions.append(transaction)
+    return transactions
