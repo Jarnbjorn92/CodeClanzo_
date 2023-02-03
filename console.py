a25eb7a3fea92e8341048b2581f4ee4f479b95ad
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-02-03 14:02:59 +0000

    Added all user functions to user_repository and tested they work in console

--- console.py
+++ console.py
@@ -4,3 +4,11 @@ from models.transaction import Transaction
 import repositories.transaction_repository as transaction_repository
 import repositories.user_repository as user_repository
 
+# user_1 = User('Connor', 'Fleming')
+# user_repository.save(user_1)
+# TESTED AND WORKS. 
+
+# user_repository.delete(1)
+# TESTED AND WORKS.
+
+
--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -2,3 +2,9 @@ from db.run_sql import run_sql
 
 from models.user import User
 from models.transaction import Transaction
+
+import repositories.user_repository as user_repository
+
+
+def save(transaction):
+    sql = "INSERT"
