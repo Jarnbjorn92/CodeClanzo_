8be3617d80e83ac3768bab93dac29cd39070992a
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-02-03 13:26:46 +0000

    partially setup controller. Set up run_sql. created repository files and partially set them up

---
+++ controllers/transactions_controller.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.transaction import Transaction
+
+
+# NEW
+# GET '/transactions/new'
+
+# CREATE
+# POST '/transactions'
+
+# SHOW
+# GET '/transactions/<id>'
+
+# EDIT
+# GET '/transactions/<id>/edit'
+
+# UPDATE
+# PUT '/transactions/<id>'
+
+# DELETE
+# DELETE '/transactions/<id>'
