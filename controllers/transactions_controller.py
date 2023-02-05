a08da5bde3cab066a7f3d21e11f832a37018338b
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-02-05 16:01:34 +0000

    Created a new transaction page. Routing the post to create soon

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.transaction import Transaction
+from models.user import User
 import repositories.user_repository as user_repository
 import repositories.transaction_repository as transaction_repository
 
 transactions_blueprint = Blueprint("transactions", __name__)
 
 @transactions_blueprint.route('/transactions')
-def transactions():
+def show_transactions():
     transactions = transaction_repository.select_all()
     return render_template("transactions/index.html", all_transactions = transactions)
 
 # NEW
 # GET '/transactions/new'
-
+@transactions_blueprint.route("/transactions/new")
+def new_transaction():
+    transactions = transaction_repository.select_all()
+    return render_template('transactions/new.html', transactions = transactions)
 
 # CREATE
 # POST '/transactions'
+
+
 
 # SHOW
 # GET '/transactions/<id>'
@@ -33,4 +39,3 @@ def transactions():
 def delete_book(id):
     transaction_repository.delete(id)
     return redirect('/transactions')
-    
