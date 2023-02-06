27b8234bc062f8b3be5e5a370f76599c981ada81
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-02-06 11:59:37 +0000

    added delete user function. not working. need to check routing

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -8,11 +8,22 @@ users_blueprint = Blueprint("users", __name__)
 
 @users_blueprint.route('/users')
 def users():
-    users = user_repository.select_all
+    users = user_repository.select_all()
     return render_template("users/index.html", all_users = users)
-
-# NEW
-# GET '/users/new'
 
 # CREATE
 # POST '/users'
+@users_blueprint.route('/users', methods=['POST'])
+def create_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    user = User(first_name,last_name)
+    user_repository.save(user)
+    return redirect('/users')
+
+# DELETE
+# POST '/users/<id>/delete
+@users_blueprint.route('/users/<id>/delete', methods=['POST'])
+def delete_user(id):
+    user_repository.delete(id)
+    return redirect('/users')
